88be14c92d798d55dd1a55b7b8fcd8021755cc00
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-09-14 18:26:10 +0500

    a

--- Task3/HMACCalculator.cs
+++ Task3/HMACCalculator.cs
@@ -7,7 +7,7 @@ public class HmacCalculator
 {
     public string CalculateHmac(string message, byte[] key)
     {
-        using (var hmac = new HMACSHA256(key)) // You can replace HMACSHA256 with HMACSHA3 if available
+        using (var hmac = new HMACSHA256(key))
         {
             byte[] messageBytes = Encoding.UTF8.GetBytes(message);
             byte[] hash = hmac.ComputeHash(messageBytes);
